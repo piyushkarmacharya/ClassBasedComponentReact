e2ef8ee6a04b15f2602ef0e8ea805c98301fbfa6
Piyush Karmacharya <piyushkarmacharya65@gmail.com>
2024-06-30 23:27:07 +0545

    get data from api then changed favicon

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -11,7 +11,7 @@ export default class NewsItem extends Component {
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href={url} className="btn btn-primary">Visit site</a>
+            <a href={url} target='_blank' className="btn btn-primary">Visit site</a>
           </div>
         </div>
       </>
